50e7d721d7347b63873a7bfae279ba743edc5581
Glitch (dnd-spells) <none>
2020-08-13 01:04:17 +0000

    added all the tsvs and made sql endpoint

--- server.js
+++ server.js
@@ -18,3 +18,45 @@ db.all("SELECT * FROM Classes_Spells LEFT OUTER JOIN Spells ON Spells.id = spell
 var listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
+
+
+app.get("/sql/:table", (request, response) => {
+  let table = request.params.table 
+  console.log(`sending json for table ${table}`)
+  db.all(`SELECT * FROM ${table}`, (err, rows) => {
+    if (err) console.log(err)
+    response.send(htmlTableFrom(tableifiedObjArray(rows)))
+  });
+})
+
+
+function tableifiedObjArray(array) {
+  /* turns an array with structure like:
+       [{name: "Art",     lvl:0,  Desc:"Failing"}, 
+        {name: "Maths",   lvl:4,  Desc:"Exceptional"}
+        {name: "Science", lvl:2,  Desc:"Needs to do more homework"}] 
+    into an array with structure like:
+       [["name",    "lvl",  "Desc"],
+        ["Art",     0,      "Failing"],
+        ["Maths",   4,      "Exceptional"]
+        ["Science", 2,      "Needs to do more homework"]] */
+  
+  let headers = Object.keys(array[0])
+  let table = []
+  table.push(headers)
+  for (row of array) {
+    table.push(headers.map(key=>row[key]))
+  }
+  return table
+}
+
+function htmlTableFrom(array) {
+  // [["a", "b"], ["c", "d"]] --> '<table><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr></table>'
+  return "<table>" +
+    ["",...array].reduce((full,row)=>full+
+      "<tr><td>"+
+          row.join("</td><td>")+
+      "</td></tr>"
+      )+
+  "</table>"
+}
